bb0977d208923f9a2fb233b45447a06623db49b4
Jörg <joerg@lauenroth.org>
2022-02-25 22:37:09 +0100

    floating button

--- src/pages/time-tracking.tsx
+++ src/pages/time-tracking.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { FloatingButton } from '../components/basics/FloatingButton/FloatingButton';
 import { MainLayout } from '../components/layout/MainLayout/MainLayout';
 
 export default function TimeTrackingPage() {
@@ -11,6 +12,7 @@ export default function TimeTrackingPage() {
       <MainLayout className="relative grid gap-6 px-3 py-6 sm:gap-5 sm:p-5">
         <h1 className="text-3xl font-bold">Time tracking</h1>
       </MainLayout>
+      <FloatingButton />
     </>
   );
 }
