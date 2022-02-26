4d82fa5fb0fc7eacba81966d73bd3062d6183079
Jörg <joerg@lauenroth.org>
2022-02-26 23:32:53 +0100

    floating button

--- src/components/basics/FloatingButton/FloatingButton.tsx
+++ src/components/basics/FloatingButton/FloatingButton.tsx
@@ -1,18 +1,29 @@
 import { FC } from 'react';
+import styled from 'styled-components';
 
-const FloatingButton: FC = () => {
+interface FloatingButtonProps {
+  onClick: () => void;
+}
+
+const FloatingButton: FC<FloatingButtonProps> = ({ onClick }) => {
   return (
-    <div>
-      <button
-        className="fixed bottom-0 right-0 mb-4 mr-4 p-4 bg-blue-500 text-white rounded-full shadow-lg"
-        onClick={() => console.log('click')}
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" width="16" height="16" viewBox="0 0 24 24">
-          <path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z" />
-        </svg>
-      </button>
-    </div>
+    <Wrapper className="fixed bottom-0 right-0 mb-4 mr-4 p-4 rounded-full shadow-lg" onClick={onClick}>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" width="16" height="16" viewBox="0 0 24 24">
+        <path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z" />
+      </svg>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.button`
+  background-color: #6466f0;
+  opacity: 0.8;
+  transition: opacity 0.25s ease-in-out;
+
+  &:hover,
+  &:focus {
+    opacity: 1;
+  }
+`;
 
 export { FloatingButton };
