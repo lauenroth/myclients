1ad06c95b4e57c60635c9b16fd71451e9f83a98b
Jörg <joerg@lauenroth.org>
2022-03-06 10:35:26 +0100

    custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,7 @@
+export default function FourOhFour() {
+  return (
+    <section className="grid place-content-center" style={{ height: '85vh' }}>
+      <h1 className="text-2xl md:text-5xl">404 - Page Not Found</h1>
+    </section>
+  );
+}
