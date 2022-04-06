f323efcce691651eb8683fa56d3e08c19fb42b17
Jörg <joerg@lauenroth.org>
2022-04-06 07:42:55 +0200

    icons in user menu

--- src/components/layout/MainNavigation/MainNavigation.tsx
+++ src/components/layout/MainNavigation/MainNavigation.tsx
@@ -18,13 +18,42 @@ const navigation = [
   { name: 'Time Tracking', href: '/time-tracking', slug: 'timeTracking' },
 ];
 const userNavigation = [
-  { name: 'Your Profile', href: '/profile' },
-  { name: 'Settings', href: '/settings' },
-  { name: 'Sign out', href: '#logout' },
+  { name: 'Your Profile', href: '/profile', icon: 'profile' },
+  { name: 'Settings', href: '/settings', icon: 'settings' },
+  { name: 'Sign out', href: '#logout', icon: 'logout' },
 ];
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
+}
+
+function getSVG(type: string) {
+  switch (type) {
+    case 'profile':
+      return (
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-2 opacity-75" viewBox="0 0 20 20" fill="currentColor">
+          <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
+        </svg>
+      );
+    case 'settings':
+      return (
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-2 opacity-75" viewBox="0 0 20 20" fill="currentColor">
+          <path d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zM11 4a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zM16 3a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z" />
+        </svg>
+      );
+    case 'logout':
+      return (
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-2 opacity-75" viewBox="0 0 20 20" fill="currentColor">
+          <path
+            fillRule="evenodd"
+            d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
+            clipRule="evenodd"
+          />
+        </svg>
+      );
+    default:
+      return null;
+  }
 }
 
 const MainNavigation = () => {
@@ -96,7 +125,8 @@ const MainNavigation = () => {
                           <Menu.Item key={item.name}>
                             {({ active }) => (
                               <Link href={item.href}>
-                                <a className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
+                                <a className={classNames(active ? 'bg-gray-100' : '', 'flex px-4 py-2 text-sm text-gray-700')}>
+                                  {getSVG(item.icon)}
                                   {item.name}
                                 </a>
                               </Link>
